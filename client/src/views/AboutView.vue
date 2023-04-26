b255954f304b454d034b164d4d957bc5d0a9368e
Doffy So <bitvapesen@gmail.com>
2023-04-26 14:11:23 +0300

    First commit

---
+++ client/src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<script>
+import AuthenticationService from '../services/AuthenticationService';
+import axios from 'axios';
+
+// const res = await AuthenticationService.register({
+//   name: this.name
+// })
+// console.log(res.data)
+
+export default {
+  data() {
+    return {
+      name: "",
+    }
+  }, 
+  methods: {
+    async register() {
+      AuthenticationService.register({
+        name: this.name
+      })
+    }
+  },
+  watch: {
+
+  }
+}
+</script>
+
+<template>
+  <div class="about">
+      <input v-model="name" type="text" name="name">
+      <button @click="register">Click!</button>
+  </div>
+</template>
