90ef8b35748b850ed45ac235d02aac760dfe62e2
Doffy So <bitvapesen@gmail.com>
2023-05-02 08:47:59 +0300

    Fixed styles and adaptivity

--- src/components/TheHeader.php
+++ src/components/TheHeader.php
@@ -10,7 +10,7 @@ $header = "FortNoks Club";
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="src/styles/header.css">
     <link rel="stylesheet" href="src/styles/index.css">
     <link rel="stylesheet" href="src/styles/media.css">
